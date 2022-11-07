f076ae73bb67778a53780a03167120630b4be7a6
Christian Siaton <cds116@georgetown.edu>
2022-11-07 17:55:17 -0500

    Refactor and tested stocks data fetching logic

--- app/stocks.py
+++ app/stocks.py
@@ -1,5 +1,5 @@
-
 # this is the "app/stocks.py" file...
+
 
 from pandas import read_csv
 
@@ -9,6 +9,15 @@ def format_usd(my_price):
     return f"${my_price:,.2f}"
 
 
+def fetch_stocks_data(symbol):
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&datatype=csv"
+
+    df = read_csv(request_url)
+
+    return df
+
+
+
 if __name__ == "__main__":
 
     print("STOCKS REPORT...")
@@ -16,9 +25,8 @@ if __name__ == "__main__":
     symbol = input("Please input a crypto symbol (default: 'NFLX'): ") or "NFLX"
     print("SYMBOL:", symbol)
 
-    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&datatype=csv"
+    df = fetch_stocks_data(symbol)
 
-    df = read_csv(request_url)
     print(df.columns)
     print(df.head())
     #breakpoint()
